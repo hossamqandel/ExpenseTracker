923f7d37e0c7f2973ad39a6958018f750e330848
Hossam <hossam.sayed@passboard.net>
2023-08-31 04:56:16 +0300

    feature: Set app theme based on last user choices when user open the app

--- app/src/main/java/dev/hossam/expensetracker/BaseApplication.kt
+++ app/src/main/java/dev/hossam/expensetracker/BaseApplication.kt
@@ -1,6 +1,9 @@
 package dev.hossam.expensetracker
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
 import dev.hossam.expensetracker.core.data.sharedpref.SharedPrefUtil
 
 class BaseApplication : Application() {
@@ -8,7 +11,15 @@ class BaseApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         SharedPrefUtil.init(this)
+        setAppTheme()
     }
 
+    private fun setAppTheme(){
+        val isLight = SharedPrefUtil.getAppTheme()
+        when(isLight){
+            true -> AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
+            false -> AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
+        }
+    }
 
 }
